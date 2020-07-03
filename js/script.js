bec8f2e765925b9bbff29edbdce95bcdb8eee15c
fkma13552 <ozonplay123@gmail.com>
2020-07-03 20:32:23 +0300

    4 hours

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNavBar").style.width = "30%";
+    document.getElementById("myFooter").style.width = "30%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNavBar").style.width = "0";
+    document.getElementById("myFooter").style.width = "0";
+  }
